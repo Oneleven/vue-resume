5a9ee6a8d462a5039fc7658908459cd5c53c271e
Oneleven <wuduyouwo@gmail.com>
2017-07-23 20:59:43 +0800

    重构个人信息和工作经历组件

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -11,20 +11,51 @@
         </nav>
         <div class="tab_content">
             <ol>
-                <li v-for="i in [0,1,2,3,4,5]" v-bind:class="{active:currentTab===i}">tab1</li>
+                <li v-bind:class="{active:currentTab===0}">
+                    <Production v-bind:profile="profile" />
+                </li>
+                <li v-bind:class="{active:currentTab===1}">
+                    <h3>学习经历</h3>
+                </li>
+                <li v-bind:class="{active:currentTab===2}">
+                    <JobHistory v-bind:workHistory="workHistory"/>
+                </li>
+                <li v-bind:class="{active:currentTab===3}">
+                    <h3>项目经历</h3>
+                </li>
+                <li v-bind:class="{active:currentTab===4}">
+                    <h3>技能掌握</h3>
+                </li>
+                <li v-bind:class="{active:currentTab===5}">
+                    <h3>联系方式</h3>
+                </li>
             </ol>
         </div>
     </div>
 </template>
 
 <script>
+import Production from './Production'
+import JobHistory from './JobHistory'
+
 export default {
+    components: {
+        Production,JobHistory
+    },
     data() {
         return {
             currentTab: 0,
-            icons: ["production", "study", "job", "skills", "project", "phone"]
+            icons: ["production", "study", "job", "skills", "project", "phone"],
+            profile: {
+                name: "",
+                city: "",
+                birth: ""
+            },
+            workHistory: [
+                { company: '', content: '' }
+            ]
         }
-    }
+    },
 }
 </script>
 
@@ -54,6 +85,8 @@ export default {
         }
     }
     .tab_content {
+        overflow: auto;
+        width: 100%;
         li {
             display: none;
             &.active {
---
+++ src/components/JobHistory.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>工作经历</h3>
+        <el-form>
+            <div v-for="(work,index) in workHistory">
+                <el-form-item label="公司">
+                    <el-input v-model="work.company"></el-input>
+                </el-form-item>
+                <el-form-item label="工作内容">
+                    <el-input v-model="work.content"></el-input>
+                </el-form-item>
+                <i class="el-icon-delete" v-on:click="removeWorkHistory(index)"></i>
+                <hr>
+            </div>
+            <el-button v-on:click="addWorkHistory">添加</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  props:['workHistory'],
+  methods: {
+        addWorkHistory() {
+            this.workHistory.push({
+                company: '', content: ''
+            })
+        },
+        removeWorkHistory(index) {
+            this.workHistory.splice(index, 1)
+        }
+    }
+}
+</script>
+
